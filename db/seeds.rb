270bda2db94034d4c17040d4af85065586f6402e
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-02-19 15:16:30 -0600

    postgres gem, letter opener and devise modified
    -devise root change
    -clients authentication added
    -users header modified
    -postgres and letter opener added
    -seed of users created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create( email:"user1@test", password:"usuario1", password_confirmation:"usuario1" )
+User.create( email:"user2@test", password:"usuario2", password_confirmation:"usuario2" )
+User.create( email:"user3@test", password:"usuario3", password_confirmation:"usuario3" )
+User.create( email:"user4@test", password:"usuario4", password_confirmation:"usuario4" )
