b1d83cb3ed4adc20893f0be3811cb1f3fb48bdf9
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-03-15 14:14:38 -0600

    Tickets controller and views changed
    these were changed to work as nested resources

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,18 +1,16 @@
 class TicketsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_client
   before_action :set_ticket, only: %i[ show edit update destroy ]
 
-  # GET /tickets or /tickets.json
-  def index
-    @tickets = Ticket.all
-  end
+  layout 'devise'
 
-  # GET /tickets/1 or /tickets/1.json
   def show
   end
 
   # GET /tickets/new
   def new
-    @ticket = Ticket.new
+    @ticket = @client.tickets.build
   end
 
   # GET /tickets/1/edit
@@ -21,15 +19,14 @@ class TicketsController < ApplicationController
 
   # POST /tickets or /tickets.json
   def create
-    @ticket = Ticket.new(ticket_params)
-
+    @ticket = @client.tickets.build(ticket_params)
     respond_to do |format|
       if @ticket.save
-        format.html { redirect_to ticket_url(@ticket), notice: "Ticket was successfully created." }
-        format.json { render :show, status: :created, location: @ticket }
+        format.html { redirect_to client_ticket_path(@client, @ticket), notice: "Ticket was successfully created." }
+        format.json { render :show, status: :created, location: [@client, @ticket] }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+        format.json { render json: [@client, @ticket].errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,11 +35,11 @@ class TicketsController < ApplicationController
   def update
     respond_to do |format|
       if @ticket.update(ticket_params)
-        format.html { redirect_to ticket_url(@ticket), notice: "Ticket was successfully updated." }
-        format.json { render :show, status: :ok, location: @ticket }
+        format.html { redirect_to client_ticket_path([@client, @ticket]), notice: "Ticket was successfully updated." }
+        format.json { render :show, status: :ok, location: [@client, @ticket] }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+        format.json { render json: [@client, @ticket].errors, status: :unprocessable_entity }
       end
     end
   end
@@ -52,15 +49,19 @@ class TicketsController < ApplicationController
     @ticket.destroy!
 
     respond_to do |format|
-      format.html { redirect_to tickets_url, notice: "Ticket was successfully destroyed." }
+      format.html { redirect_to @client, notice: "Ticket was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_client
+      @client = Client.find(params[:client_id])
+    end
+
     def set_ticket
-      @ticket = Ticket.find(params[:id])
+      @ticket = @client.tickets.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
