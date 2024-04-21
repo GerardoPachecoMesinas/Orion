5a7090667be5c4c6962d38f9cc6e81888a274287
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-04-20 22:37:57 -0600

    Update app/controllers/reports_controller.rb
    Co-authored-by: AlfredoJA <alfre.jangeles@hotmail.com>

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,7 +4,7 @@ class ReportsController < ApplicationController
   def monthly_income_report
     start_date = DateTime.current.beginning_of_month
     end_date = DateTime.current.end_of_month
-    @paid_services = Service.where("paid_at >= ? AND paid_at <= ?", start_date, end_date)
+    @paid_services = Service.where(paid_at: start_date..end_date)
     @total_income = @paid_services.sum(:price)
   end
   
