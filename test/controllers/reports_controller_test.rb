cce1538c1bc4f454c4b52202f11562698a75a632
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-04-22 15:54:01 -0600

    Report test file deleted

--- test/controllers/reports_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ReportsControllerTest < ActionDispatch::IntegrationTest
-  test "should get monthly_income_report" do
-    get reports_monthly_income_report_url
-    assert_response :success
-  end
-end
