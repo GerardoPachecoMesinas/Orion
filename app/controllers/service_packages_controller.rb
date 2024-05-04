1ff14833258a27fb4dfa93392a3b57897baec6fc
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-05-03 21:05:23 -0600

    Revert "authenticate_user was added and comments deleted"
    This reverts commit 027a53429a858439c3fc375515214d87ec9e0dc6.

--- app/controllers/service_packages_controller.rb
+++ app/controllers/service_packages_controller.rb
@@ -1,21 +1,25 @@
 class ServicePackagesController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_service_package, only: %i[ show edit update destroy ]
 
+  # GET /service_packages or /service_packages.json
   def index
     @service_packages = ServicePackage.all
   end
 
+  # GET /service_packages/1 or /service_packages/1.json
   def show
   end
 
+  # GET /service_packages/new
   def new
     @service_package = ServicePackage.new
   end
 
+  # GET /service_packages/1/edit
   def edit
   end
 
+  # POST /service_packages or /service_packages.json
   def create
     @service_package = ServicePackage.new(service_package_params)
 
@@ -30,6 +34,7 @@ class ServicePackagesController < ApplicationController
     end
   end
 
+  # PATCH/PUT /service_packages/1 or /service_packages/1.json
   def update
     respond_to do |format|
       if @service_package.update(service_package_params)
@@ -42,6 +47,7 @@ class ServicePackagesController < ApplicationController
     end
   end
 
+  # DELETE /service_packages/1 or /service_packages/1.json
   def destroy
     @service_package.destroy!
 
@@ -52,10 +58,12 @@ class ServicePackagesController < ApplicationController
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_service_package
       @service_package = ServicePackage.find(params[:id])
     end
 
+    # Only allow a list of trusted parameters through.
     def service_package_params
       params.require(:service_package).permit(:name, :price, :bandwidth, :clasification)
     end
