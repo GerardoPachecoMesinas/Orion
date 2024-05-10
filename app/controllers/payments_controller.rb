39f74d1aafe60f9563d4f3c67ded61e998b702ca
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-05-09 19:20:04 -0600

    Modifying controller to nested resource

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,83 @@
+class PaymentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_client, except: :index
+  before_action :set_invoice, except: :index
+  before_action :set_payment, only: %i[ show edit update destroy ]
+
+  # GET /payments or /payments.json
+  def index
+    @payments = Payment.all
+  end
+
+  # GET /payments/1 or /payments/1.json
+  def show
+  end
+
+  # GET /payments/new
+  def new
+    @payment = @invoice.payments.build
+  end
+
+  # GET /payments/1/edit
+  def edit
+  end
+
+  # POST /payments or /payments.json
+  def create
+    @payment = Payment.new(payment_params)
+
+    respond_to do |format|
+      if @payment.save
+        format.html { redirect_to payment_url(@payment), notice: "Payment was successfully created." }
+        format.json { render :show, status: :created, location: [@invoice, @payment] }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @payment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /payments/1 or /payments/1.json
+  def update
+    respond_to do |format|
+      if @payment.update(payment_params)
+        format.html { redirect_to payment_url(@payment), notice: "Payment was successfully updated." }
+        format.json { render :show, status: :ok, location: [@invoice, @payment] }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @payment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /payments/1 or /payments/1.json
+  def destroy
+    @payment.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to payments_url(@invoice), notice: "Payment was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_client
+      @client = Client.find(params[:client_id])
+    end
+
+  # Use callbacks to share common setup or constraints between actions.
+    def set_invoice
+      @invoice = @client.invoices.find(params[:invoice_id])
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_payment
+      @payment = @invoice.payment.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def payment_params
+      params.require(:payment).permit(:piad_at, :amount, :method, :notes, :invoice_id)
+    end
+end
