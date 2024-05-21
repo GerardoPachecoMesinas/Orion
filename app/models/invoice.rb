0b0a75395d2063ed77ef96e326d2aba48baea8bf
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-05-20 23:39:57 -0600

    Add validations for payment and invoice models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,8 @@
 class Invoice < ApplicationRecord
   belongs_to :client
   belongs_to :service_package
-  has_many :payments
+  has_many :payments, dependent: :destroy
+  validates :started_at, presence: { :message => "no puede estar vacio"}
+  validates :disconnected_at, presence: { :message => "no puede estar vacio"}
+  validates :finished_at, presence: { :message => "no puede estar vacio"}
 end
