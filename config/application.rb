8a76d7b1dc8de5bd2a269aa2609d8bd08dd2c5f6
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-05-21 14:30:13 -0600

    Devise msjs translated and time_zone setted

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Orion
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.time_zone = "Mexico City"
+    config.i18n.default_locale = :es
 
     # Configuration for the application, engines, and railties goes here.
     #
