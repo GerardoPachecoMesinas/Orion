af72e442fcb9be973fb1ef639b5958de6df67c96
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-05-21 18:23:10 -0600

    update format for hash of validates

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :client
   belongs_to :service_package
   has_many :payments, dependent: :destroy
-  validates :started_at, presence: { :message => "no puede estar vacio"}
-  validates :disconnected_at, presence: { :message => "no puede estar vacio"}
-  validates :finished_at, presence: { :message => "no puede estar vacio"}
+  validates :started_at, presence: { message: "no puede estar vacio" }
+  validates :disconnected_at, presence: { message: "no puede estar vacio" }
+  validates :finished_at, presence: { message: "no puede estar vacio" }
 end
