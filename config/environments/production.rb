ba73779c859b4d9a868feaba834553bc780073af
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-05-28 19:53:03 -0600

    Update config/environments/production.rb
    Co-authored-by: Abel Pacheco <abelpacheco@outlook.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
     port: 587,
     domain: "gmail.com",
     user_name: ENV["GMAIL_USER"],
-     password: ENV["GMAIL_USER_PASSWORD"],
+    password: ENV["GMAIL_USER_PASSWORD"],
     authentication: "plain",
     enable_starttls_auto: true,
     open_timeout: 5,
