c4e3e6e0e15d097d03a57f2ee331fa253ea6010b
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-06-03 14:14:11 -0600

    add update of invoice, from invoice and payment models

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,21 @@ class Invoice < ApplicationRecord
   belongs_to :client
   belongs_to :service_package
   has_many :payments, dependent: :destroy
+
   validates :started_at, presence: { message: "no puede estar vacio" }
   validates :disconnected_at, presence: { message: "no puede estar vacio" }
   validates :finished_at, presence: { message: "no puede estar vacio" }
+
+  before_save :update_payment_status
+
+  def update_payment_status
+    total_payments = payments.sum(:amount)
+    if total_payments >= total_bill
+      self.payment_status = "Pagado"
+    elsif total_payments < total_bill && total_payments > 0
+      self.payment_status = "Parcialmente Pagado"
+    else
+      self.payment_status = "Pendiente"
+    end
+  end
 end
