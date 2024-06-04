855d8a76a008b30996cb10024231808162937c55
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-06-04 16:03:50 -0600

    Alfredo's suggestions

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,5 +1,9 @@
 module InvoicesHelper
   def service_package_options
-    ServicePackage.all.map { |sp| [sp.name, sp.id, { data: { name: sp.name, cost: sp.price } }] }
+    ServicePackage.all.map do |service_package|
+      [service_package.name,
+       service_package.id,
+       { data: { name: service_package.name, cost: service_package.price } }]
+    end
   end
 end
