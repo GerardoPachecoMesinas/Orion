eedc1c9b93cf133c85f543fbefd8403b98c5118c
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-06-04 15:27:24 -0600

    Moving method to correct helper

---
+++ app/helpers/invoices_helper.rb
@@ -0,0 +1,5 @@
+module InvoicesHelper
+  def service_package_options
+    ServicePackage.all.map { |sp| [sp.name, sp.id, { data: { name: sp.name, cost: sp.price } }] }
+  end
+end
--- app/helpers/service_packages_helper.rb
+++ app/helpers/service_packages_helper.rb
@@ -1,5 +1,2 @@
 module ServicePackagesHelper
-  def service_package_options
-    ServicePackage.all.map { |sp| [sp.name, sp.id, { data: { name: sp.name, cost: sp.price } }] }
-  end
 end
