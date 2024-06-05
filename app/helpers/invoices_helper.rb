feb1288c6e2fc43b9fe076f83ab1e752b7923d40
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-06-04 22:59:25 -0600

    Update app/helpers/invoices_helper.rb
    Co-authored-by: Abel Pacheco <abelpacheco@outlook.com>

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,9 +1,11 @@
 module InvoicesHelper
   def service_package_options
     ServicePackage.all.map do |service_package|
-      [service_package.name,
-       service_package.id,
-       { data: { name: service_package.name, cost: service_package.price } }]
+      [
+        service_package.name,
+        service_package.id,
+        { data: { name: service_package.name, cost: service_package.price } }
+      ]
     end
   end
 end
