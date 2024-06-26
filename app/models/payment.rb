ee12bbe2d47c9a8a1201f111969bba4103898f97
GerardoPachecoMesinas <136499369+GerardoPachecoMesinas@users.noreply.github.com>
2024-06-26 17:50:44 -0600

    update payment's model to work whit new invoice's methods

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,12 +4,17 @@ class Payment < ApplicationRecord
   validates :amount, presence: { message: "no puede estar vacio" }
   validates :method, presence: { message: "no puede estar vacio" }
 
-  after_save :update_invoice_payment_status
+  after_save :add_payment_to_invoice
+  after_destroy :remove_payment_from_invoice
 
   private
-
-  def update_invoice_payment_status
+  def add_payment_to_invoice
     invoice.set_payment_status
+    invoice.save
+  end
+
+  def remove_payment_from_invoice
+    invoice.reset_payment_status
     invoice.save
   end
 end
